540f897d60289ceb6505a53e01f6e323c1008e6e
Yi Wen <ywen.github@gmail.com>
2013-06-20 11:32:27 -0500

    New Rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeezeoDashboard::Application.config.secret_token = '782e07e29292da86e09db9d234a44f739ede7b8c93f76d7909698d23a8e1f13de7c9923fd354f5966f4abef02ce9003b9b788609a03000a539615549470e0e3f'
