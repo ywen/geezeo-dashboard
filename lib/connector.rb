620531e300380b73b1052da9a7e4f274f8409241
Yi Wen <ywen.github@gmail.com>
2013-06-21 16:15:17 -0500

    Finish Connector

--- lib/connector.rb
+++ lib/connector.rb
@@ -1,8 +1,8 @@
 require 'yajl'
 module Connector
   class << self
-    def get(kind)
-      Yajl::Parser.parse(class_name(kind).new.fetch.body, symbolize_keys: true)
+    def get(kind, *args)
+      Yajl::Parser.parse(class_name(kind).fetch(*args).body, symbolize_keys: true)
     end
 
     #https://{api_key}@beta-geezeosandbox.geezeo.com/api/v1/users/{user_id}/accounts
@@ -16,9 +16,34 @@ module Connector
   end
 
   class Account
+    class << self
+      def fetch
+        Fetcher.fetch
+      end
+    end
   end
 
   class Transaction
+    class << self
+      def fetch(account_id)
+        Fetcher.fetch("/#{account_id}/transactions")
+      end
+    end
+  end
+
+  class Fetcher
+    class << self
+      def fetch(extra="")
+        HTTParty.get("#{base_url}#{extra}")
+      end
+
+      private
+
+      def base_url
+        credentials = YAML.load_file("#{Rails.root}/config/credentials.yml")
+        "https://#{credentials['api_key']}@beta-geezeosandbox.geezeo.com/api/v1/users/#{credentials['user_id']}/accounts"
+      end
+    end
   end
 
 end
--- spec/lib/connector_spec.rb
+++ spec/lib/connector_spec.rb
@@ -4,20 +4,79 @@ describe Connector do
   describe ".get" do
     let(:body) { { attr: "value" } }
     let(:response) { double :response, body: body.to_json }
-    let(:connector) { double :connector, fetch: response }
 
     before(:each) do
-      Connector::Account.stub(:new).and_return connector
+      Connector::Account.stub(:fetch).and_return response
     end
 
     it "gets the right kind of contents" do
-      Connector::Account.should_receive(:new).and_return connector
+      Connector::Account.should_receive(:fetch).and_return response
       Connector.get :accounts
+    end
+
+    context "when extra params are passed in" do
+      it "passes down to the actual connector" do
+        Connector::Transaction.should_receive(:fetch).with(12).and_return response
+        Connector.get :transactions, 12
+      end
     end
 
     it "returns the result in hash" do
       expect(Connector.get(:accounts)).to eq(body)
     end
 
+  end
+end
+
+module Connector
+  describe Account do
+    describe ".fetch" do
+      it "asks the fetcher to fetch the results" do
+        Fetcher.should_receive(:fetch)
+        Account.fetch
+      end
+    end
+  end
+
+  describe Transaction do
+    describe ".fetch" do
+      let(:account_id) { 12 }
+
+      it "asks the fetcher to fetch the results" do
+        Fetcher.should_receive(:fetch).with("/12/transactions")
+        Transaction.fetch(account_id)
+      end
+    end
+  end
+
+  describe Fetcher do
+    describe ".fetch" do
+      let(:credentials) { { "api_key" => "api_key", "user_id" => "user_id" } }
+
+      before(:each) do
+        HTTParty.stub(:get).and_return "response"
+        YAML.stub(:load_file).and_return credentials
+      end
+
+      it "fetches result" do
+        HTTParty.should_receive(:get).with(
+          "https://api_key@beta-geezeosandbox.geezeo.com/api/v1/users/user_id/accounts"
+        )
+        Fetcher.fetch
+      end
+
+      it "returns the result" do
+        expect(Fetcher.fetch).to eq("response")
+      end
+
+      context "with extra query string" do
+        it "connects it" do
+          HTTParty.should_receive(:get).with(
+            "https://api_key@beta-geezeosandbox.geezeo.com/api/v1/users/user_id/accounts/extra"
+          )
+          Fetcher.fetch("/extra")
+        end
+      end
+    end
   end
 end
